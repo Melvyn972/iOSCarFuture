f54fe18ceec6b3b8cdce244134f53c060b84d992
melvyn972 <melvyn.thierrybellefond@gmail.com>
2025-11-04 13:44:38 +0100

    Initial Commit

---
+++ iOSCarFuture/iOSCarFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCarFutureApp.swift
+//  iOSCarFuture
+//
+//  Created by THIERRY-BELLEFOND Melvyn on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSCarFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
